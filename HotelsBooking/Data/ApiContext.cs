397856f31ab9afa0c86c34184a27ef8190fc94a4
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2025-04-28 17:15:21 +0300

    Add Context

---
+++ HotelsBooking/Data/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelsBooking.Models;
+
+namespace HotelsBooking.Data
+{
+    public class ApiContext :DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) 
+            :base(options) { }
+    }
+}
