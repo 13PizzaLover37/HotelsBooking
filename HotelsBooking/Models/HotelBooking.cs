1095d985f932a9cb4c6235a5e2016ac7f2ae7e16
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2025-04-28 17:06:26 +0300

    Add 'HotelBooking' model

---
+++ HotelsBooking/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelsBooking.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string Room { get; set; } = string.Empty;
+        public string ClientEmail { get; set; } = string.Empty;
+    }
+}
